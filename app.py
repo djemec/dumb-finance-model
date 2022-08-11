5e17ffecb39d27acbef4b963d77aacbeb0e8ce74
Domen Jemec <domen.jemec@gmail.com>
2022-08-10 23:10:21 -0700

    dfm

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+
+from utils import run_model, return_survived_df, plot_stats
+from utils import MAX_YEARS, MIN_AMOUNT_TOLERABLE, INFLATION_ON, MONTHLY_MIN, MONTHLY_MAX, TAX, MODELS, DEFAULT_MODEL
+
+
+st.title('Dumb Finance Model 🤑💰')
+st.markdown('_Runs a model of investing your principle in the stock you pick with the withdrawal you select._')
+st.markdown('_Nothing in this app constitutes professional and/or financial advice._')
+
+# prompt box
+col1, col2, col3 = st.columns(3)
+with col1:
+    stock = st.selectbox('Which Stock do you want to model', list(MODELS.keys()), index=0)
+    years = st.number_input('Years you want to live off the money', min_value=5, max_value=MAX_YEARS, value=MAX_YEARS)
+
+with col2:
+    min_tol = st.number_input('Minimum tolerable amount in bank$', min_value=0, max_value=10000000, 
+                          value=MIN_AMOUNT_TOLERABLE)
+    tax_rate = st.number_input('Tax Rate', min_value=0.0, max_value=1.0, value=TAX)
+    
+with col3:
+    month_min = st.number_input('Minimum monthly withdrawal $', min_value=0, max_value=20000, value=MONTHLY_MIN)
+    month_max = st.number_input('Maximum monthly withdrawal $', min_value=month_min, max_value=20000, value=MONTHLY_MAX)
+    inflation = st.checkbox('Include inflation', value=INFLATION_ON)
+
+#button and results
+if st.button('Click to run'):
+    text = 'Running Model...'
+    data_load_state = st.text('Running Model...')
+    run_model(MODELS[stock], years,min_tol,tax_rate,month_min, month_max, inflation, data_load_state)
+    sdf, sdf_stats = return_survived_df()
+    data_load_state.empty()
+    
+    # show summary chart
+    st.subheader('Principle needed to support different monthly withdrawals')
+    inflation = 'ON' if inflation else 'OFF'
+    desc = f'Principle needed to support monthly withdrawals from {month_min} to {month_max} \
+            for {years} years at a long term capital gains tax of {tax_rate}, with inflation {inflation} while \
+            maintaining a min bank balance of {min_tol}. This assumes the principle is fully invested in {stock}.'
+    st.markdown(desc)
+    tab1, tab2 = st.tabs(['Chart','Table'])
+
+    with tab1:
+        
+        fig = plot_stats(sdf)
+        st.pyplot(fig)
+
+    with tab2:
+        st.table(data=sdf_stats)
---
+++ utils.py
@@ -0,0 +1,204 @@
+import pandas as pd
+import pandas_datareader as data
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+import streamlit as st
+
+
+start_date = '1928-01-01'
+end_date = '2022-08-01'
+
+
+init_amount = 0
+max_amount = 30000000
+step = 100000
+monthly_step = 1000
+dump_data = True
+data_source = 'yahoo'
+result_df = pd.DataFrame(columns = ['start_date','principle','monthly','months_survive','survive'])
+
+MIN_AMOUNT_TOLERABLE = 100000
+INFLATION_ON = True
+MAX_YEARS = 40
+MONTHLY_MIN = 5000
+MONTHLY_MAX = 10000
+TAX = 0.2
+DEFAULT_MODEL = 'S&P500'
+MODELS = {'S&P500':'^GSPC','NASDAQ':'^IXIC','DOW':'^DJI','Tres10y':'^TNX','Coke':'KO','GE':'GE','IBM':'IBM'}
+DATA_LOAD_STATE = st.text('')
+
+def prep_data(source=MODELS[DEFAULT_MODEL], inflation=INFLATION_ON ):
+    # setups initial frame with rates and cpi
+    
+    print('prep started')
+    # pull raw data
+    stock = data.DataReader(source, data_source, start_date, end_date)[['Close']]
+    #pull the earliest stock date available
+    new_start = stock.index[0]
+    cpi = data.DataReader("CPIAUCNS", "fred", new_start, end_date)
+    
+    # user merger and then fill in monthly from the previous close if possible else the next close
+    df = cpi.merge(stock, how='outer', left_index=True, right_index=True)
+    df[['Close']] = df[['Close']].fillna(method='ffill').fillna(method='bfill')
+
+    # remove non first month 
+    df.dropna(inplace=True)
+
+    # set placeholders
+    df['inflation'] = 0.0
+    df['change'] = 0.0
+    df.reset_index(inplace=True)
+
+    # calculate baseline df
+    for i in range(len(df)):
+        # skip the first iteration
+        if i == 0: continue
+        # update inflation
+        if inflation:
+            p_cpi = df.at[i-1,'CPIAUCNS']
+            c_cpi = df.at[i,'CPIAUCNS']
+            df.at[i,'inflation']= (c_cpi - p_cpi)/p_cpi
+        # update percent change
+        p_close = df.at[i-1,'Close']
+        c_close = df.at[i,'Close']
+        df.at[i,'change'] = (c_close - p_close)/p_close
+    print('prep done')
+    return df
+
+def model_year(prin, start_date_index, withdrawal, min_amount_tol, df, tax=TAX, years=MAX_YEARS):
+    # models a specific year
+    
+    global result_df
+    # calculates the model year and inserts into results df
+    # returns boolean for making it to the end of the year
+    failed = False
+    s_date = df.at[start_date_index,'index'].strftime('%Y_%m_%d')
+    mnth = f'monthly_{s_date}_{prin}_{withdrawal}'
+    prcp = f'principle_{s_date}_{prin}_{withdrawal}'
+    df[mnth] = np.nan
+    df[prcp] = np.nan
+    k=0
+    end = start_date_index + years*12 +1
+    start = df.at[start_date_index,'index']
+    for i in range(start_date_index,end):
+        if k==0:
+            df.at[i,mnth]= withdrawal
+            df.at[i,prcp]= prin
+            k+=1
+            continue
+        if i > len(df):
+            break
+        p = i-1
+        # previous monthly * (1+inflation)
+        p_m = df.at[p,mnth]
+        c_i = df.at[i,'inflation']
+        c_m = p_m*(1+c_i)
+        df.at[i,mnth]= c_m
+        # (previous principle*(1+change)) - current monthly
+        p_p = df.at[p,prcp]
+        p_c = df.at[p,'Close']
+        c_c = df.at[i,'Close']
+        change =  (c_c-p_c)/p_c
+        c_p = p_p*(1+change)-(c_m/(1-tax))
+        df.at[i,prcp]= c_p
+        failed = (c_p <= min_amount_tol)
+        if failed or i == end-1:
+            res = pd.DataFrame.from_dict({'start_date': [start], 
+                                          'principle': prin, 
+                                          'monthly': withdrawal,
+                                          'months_survive': [i-start_date_index], 
+                                          'survive': [not failed]})
+            result_df = pd.concat([result_df,res], ignore_index=True)
+            break
+    if dump_data:
+        df.drop(columns=[mnth,prcp], inplace=True)
+    return failed
+
+def seek_year(sdi, p_min, p_max, step, withdrawal, min_amount_tol, baseline_df, tax, years):
+    # interates over a series of principles to find the lowest that makes it to the years amount
+    # sdi = start date index 
+    # p_min = principle min
+    # p_max = principle max
+    # step = increase step from in to max
+    
+    # presumes failed state
+    failed = True
+    for prin in range(p_min,p_max,step):
+        failed = model_year(prin, sdi, withdrawal, min_amount_tol, baseline_df, tax, years)
+        # exists when succeeds
+        if not failed:
+            break
+            
+def run_model(model = MODELS['S&P500'], years=MAX_YEARS,
+              min_amount_tol=MIN_AMOUNT_TOLERABLE, tax=TAX,
+              monthly_min=MONTHLY_MIN, monthly_max=MONTHLY_MAX, inflation= INFLATION_ON,
+              dls=DATA_LOAD_STATE):
+    # runs the modeling
+    model_df = prep_data(model)
+    lsi = int((len(model_df)- years*12)/12)
+    for s in range(lsi + 1):
+        s_date_i = s*12
+        year = model_df.at[s_date_i,'index'].strftime('%Y_%m_%d')
+        mm = monthly_max+monthly_step
+        print(f'analyzing year: {year} {s+1} of {lsi + 1}')
+        dls.text(f'progress {s+1} of {lsi + 1}')
+        for mon in range(monthly_min,mm,monthly_step):
+            seek_year(s_date_i, init_amount, max_amount, step, mon, min_amount_tol, model_df,tax, years)
+            
+def return_survived_df():
+    # filters and returns results and the distribution
+    survive_df = result_df[result_df.survive]
+    survive_df= survive_df.astype({'start_date': object, 
+                                   'principle': int, 
+                                   'monthly': int, 
+                                   'months_survive': int, 
+                                   'survive': bool})
+    
+    sdf_summary = survive_df[['principle','monthly']].groupby('monthly').describe()
+    sdf_summary.columns = [i[1] for i in sdf_summary.columns]
+    sdf_summary = sdf_summary.drop(columns='count')
+    sdf_summary.index = ['${:,}'.format(x) for x in sdf_summary.index]
+    for c in sdf_summary.columns:
+        sdf_summary[c] = sdf_summary[c].apply(lambda x: '${:,.0f}'.format((x)))
+    return survive_df,sdf_summary
+
+def return_all_df():
+    # filters and returns results
+    all_df = result_df.astype({'start_date': object, 
+                                   'principle': int, 
+                                   'monthly': int, 
+                                   'months_survive': int, 
+                                   'survive': bool})
+    return all_df
+
+def plot_stats(sdf):
+    x_l = 'monthly withdrawal'
+    y_l = 'principle'
+
+    unique_monthly = sdf.monthly.unique()
+    data =[list(sdf[sdf['monthly'] == i].principle) for i in unique_monthly]
+    fig, ax = plt.subplots()
+
+    fig.set_size_inches(10, 6)
+    ax.boxplot(data)
+    ax.set_xticklabels(['${:,}'.format(x) for x in unique_monthly])
+    ax.yaxis.set_major_formatter('${x:,.0f}')
+    ax.set_ylabel(y_l)
+    ax.set_xlabel(x_l)
+
+    return fig
+            
+            
+
+if __name__ == '__main__':
+    run_model()
+    sdf, sdf_stats = return_survived_df()
+    inf_text = 'Yes' if INFLATION_ON else 'No'
+    title = f'principle needed (inflation {inf_text}, min_bal {min_amount_tolerable}, years {MAX_YEARS}, model {default_model})'
+    x_l = 'monthly withdrawal'
+    y_l = 'Principle[$]'
+
+    sdf.plot.box(column=["principle"], by=['monthly'], title=title, ylabel=y_l, xlabel=x_l, figsize=(10,5))
+    print(sdf_stats)
+  
